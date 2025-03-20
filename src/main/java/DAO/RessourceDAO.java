e83dbc8779b00b126595504e2f48183452ebe29e
krrsalmane <karrounsalmane@gmail.com>
2025-03-20 11:13:59 +0000

    display ressource

--- src/main/java/DAO/RessourceDAO.java
+++ src/main/java/DAO/RessourceDAO.java
@@ -27,5 +27,26 @@ public class RessourceDAO {
     }
 
     // Get all resources
+    public List<Ressource> getAllRessources() {
+        List<Ressource> ressources = new ArrayList<>();
+        String sql = "SELECT * FROM ressource";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                Ressource ressource = new Ressource();
+                ressource.setId(rs.getInt("id"));
+                ressource.setNom(rs.getString("nom"));
+                ressource.setType(rs.getString("type"));
+                ressource.setQuantite(rs.getInt("quantite"));
+                ressource.setInfoFournisseur(rs.getString("info_fournisseur"));
+                ressources.add(ressource);
+            }
+        } catch (Exception e) {
+            System.out.println("Error getting resources: " + e.getMessage());
+        }
+        return ressources;
+    }
+
 
 }
